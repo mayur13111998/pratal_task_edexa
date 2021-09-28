1ed3a657f8b3eb8e8f367985752c734212c8204a
Mayur Parmar <84327752+mayur13111998@users.noreply.github.com>
2021-09-28 16:50:26 +0530

    React lazy loadding Done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
 import "./styles.css";
+import { Route, Link } from "react-router-dom";
+import React, { Suspense } from "react";
+
+const Login = React.lazy(() => import("./compnents/login/Login"));
+const Signup = React.lazy(() => import("./compnents/signup/Signup"));
+const Darshboard = React.lazy(() =>
+  import("./../compnents/dashboard/Darshboard")
+);
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-      gfggf
+      <ul>
+        <Link to="/login">login</Link>
+        <Link to="/signup">signup</Link>
+        <Link to="/darshboard">darshboard</Link>
+      </ul>
+      <Suspense fallback={<div>Please Wait.....</div>}>
+        <Route path="/login">
+          <Login />
+        </Route>
+
+        <Route path="/signup">
+          <Signup />
+        </Route>
+
+        <Route path="/darshboard">
+          <Darshboard />
+        </Route>
+      </Suspense>
     </div>
   );
 }
